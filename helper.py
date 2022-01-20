36ecd56323b12f6354efcfa91a9e540818c9c7ce
el <airnh.root@gmail.com>
2022-01-20 11:52:11 -0800

    fix

--- helper.py
+++ helper.py
@@ -4,22 +4,7 @@ import numpy as np
 import json
 ans = {}
 
-def answer1(value):
-    ans["q1"] = value
-    
-def answer2(value):
-    ans["q2"] = value
-    
-def answer3(value):
-    ans["q3"] = value
-    
-def answer4(value):
-    ans["q4"] = value
-
-def email(email):
-    ans["email"] = email
-
-    
+   
 def showResult(ans):
     # serialized = json.dumps(ans, indent=4)
     print("Question 1: ", end="")
